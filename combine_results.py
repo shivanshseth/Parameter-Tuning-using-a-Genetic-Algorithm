f9b160f8c59c22a249417f105cc40e9741db479b
Pranav174 <pranav.goyal@research.iiit.ac.in>
2020-03-27 01:06:10 +0530

    add ranking in combined

--- combine_results.py
+++ combine_results.py
@@ -10,6 +10,7 @@ for filename in os.listdir(directory):
     with open(directory+"/"+ filename,'r') as f:
         all_results.extend(json.load(f))
 all_results = sorted(all_results, key=lambda x: (0.4 * x[1][0] + 0.4 * x[1][1] + 0.2 * abs(x[1][1]-x[1][0])))
+all_results = [result+[i] for i,result in enumerate(all_results)]
 
 with open("combined_results",'w') as outfile:
     json.dump(all_results, outfile, indent=2)
